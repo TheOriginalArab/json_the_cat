5469d85cb85d6e80ad0c7728dcb8bb91006435a5
Peter Hashem <peterhashem@gmail.com>
2023-09-24 22:01:50 -0400

    refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,22 @@
 const request = require("request");
 
-const fetchBreedDescription = (breedName) => {
+const fetchBreedDescription = (breedName, callback) => {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(url, (error, response, body) => {
-    try {
-      if (error) {
-        throw error;
-      }
-
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        console.log("Breed not found");
-        return;
-      }
-
-      const description = data[0].description;
-      console.log(description);
-    } catch (error) {
-      console.error(error);
+    if (error) {
+      callback(error, null);
+      return;
     }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback(`Breed ${breedName} not found`, null);
+      return;
+    }
+
+    const breed = data[0];
+    callback(null, breed.description);
   });
 };
 
-const breedName = process.argv[2];
-
-if (!breedName) {
-  console.log("Please provide a breed name");
-  process.exit(1);
-}
-
-fetchBreedDescription(breedName);
+module.exports = { fetchBreedDescription };
